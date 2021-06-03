90e522f0b23abb8355a12312768dda880363aa60
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-06-03 19:44:16 +0800

    modified kern and user code

--- af_xdp_user2.c
+++ af_xdp_user2.c
@@ -22,31 +22,7 @@
 #include <linux/if_ether.h>
 #include <linux/ipv6.h>
 #include <linux/icmpv6.h>
-#include <linux/udp.h>
-#include <linux/ip.h>
 
-#include <stdio.h>       //For standard things
-#include <stdlib.h>      // malloc
-#include <string.h>      //memset                                                                                                                                        
-#include <netinet/ip_icmp.h>     //Provides declarations for icmp header
-//#include<netinet/udp.h> //Provides declarations for udp header
-#include <netinet/tcp.h> //Provides declarations for tcp header
-//#include <netinet/ip.h>  //Provides declarations for ip header
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <net/if.h>
-# define NO_OF_CHARS 256
-
-//static bool ProcessPacket(unsigned char* , int);
-static bool process_udp_packet(unsigned char *Buffer , int Size);
-void prefixSuffixArray(char* pat, int M, int* pps);
-void check_pattern(unsigned char *data, int Size);
-int max (int a, int b);
-void badCharHeuristic( char *str, int size, int badchar[NO_OF_CHARS]);
-int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0;
-int flag = 0;
-int pkt_count = 0;
-int pkt_dum = 0;
 
 #include "../common/common_params.h"
 #include "../common/common_user_bpf_xdp.h"
@@ -73,8 +49,6 @@ struct stats_record {
 	uint64_t rx_bytes;
 	uint64_t tx_packets;
 	uint64_t tx_bytes;
-    // we can put bool type here if true, packet matched, if false, packet did not match
-    int match[64];
 };
 
 
@@ -301,29 +275,256 @@ static inline void csum_replace2(__sum16 *sum, __be16 old, __be16 new)
 static bool process_packet(struct xsk_socket_info *xsk,
 			   uint64_t addr, uint32_t len)
 {
-	uint8_t *pkt = xsk_umem__get_data(xsk->umem->buffer, addr); // start of data
-	unsigned int payload_size;
-    unsigned char *payload;
-    struct udphdr *udp;
-    struct iphdr *ip;
-
-	struct ethhdr *eth = (struct ethhdr *) pkt;
-	//struct ethhdr *eth = pkt;
-    ip = pkt + sizeof(*eth);
-    udp = (void *)ip + sizeof(*ip);
-	payload = (unsigned char *)udp + sizeof(*udp);
-    payload_size = ntohs(udp->len) - sizeof(*udp);
-	check_pattern(payload, payload_size);
-	udp++;
-
+	uint8_t *pkt = xsk_umem__get_data(xsk->umem->buffer, addr);
+    //struct ethhdr *eth = (struct ethhdr *) pkt;
+    
+        /* Lesson#3: Write an IPv6 ICMP ECHO parser to send responses
+	 *
+	 * Some assumptions to make it easier:
+	 * - No VLAN handling
+	 * - Only if nexthdr is ICMP
+	 * - Just return all data with MAC/IP swapped, and type set to
+	 *   ICMPV6_ECHO_REPLY
+	 * - Recalculate the icmp checksum */
+	check_pattern(pkt, len);
 	if (flag == 0)
-        xsk->stats.match[pkt_dum] = 0; // pattern not found
-    else
-        xsk->stats.match[pkt_dum] = 1; // pattern found 
-	
-	pkt_dum++;
+		printf("No pattern found\n");
+	else	
+		printf("Pattern found\n");
+
+	if (false) {
+		int ret;
+		uint32_t tx_idx = 0;
+		uint8_t tmp_mac[ETH_ALEN];
+		struct in6_addr tmp_ip;
+		struct ethhdr *eth = (struct ethhdr *) pkt;
+		struct ipv6hdr *ipv6 = (struct ipv6hdr *) (eth + 1);
+		struct icmp6hdr *icmp = (struct icmp6hdr *) (ipv6 + 1);
+
+		if (ntohs(eth->h_proto) != ETH_P_IPV6 ||
+		    len < (sizeof(*eth) + sizeof(*ipv6) + sizeof(*icmp)) ||
+		    ipv6->nexthdr != IPPROTO_ICMPV6 ||
+		    icmp->icmp6_type != ICMPV6_ECHO_REQUEST)
+			return false;
+
+		memcpy(tmp_mac, eth->h_dest, ETH_ALEN);
+		memcpy(eth->h_dest, eth->h_source, ETH_ALEN);
+		memcpy(eth->h_source, tmp_mac, ETH_ALEN);
+
+		memcpy(&tmp_ip, &ipv6->saddr, sizeof(tmp_ip));
+		memcpy(&ipv6->saddr, &ipv6->daddr, sizeof(tmp_ip));
+		memcpy(&ipv6->daddr, &tmp_ip, sizeof(tmp_ip));
+
+		icmp->icmp6_type = ICMPV6_ECHO_REPLY;
+
+		csum_replace2(&icmp->icmp6_cksum,
+			      htons(ICMPV6_ECHO_REQUEST << 8),
+			      htons(ICMPV6_ECHO_REPLY << 8));
+
+		/* Here we sent the packet out of the receive port. Note that
+		 * we allocate one entry and schedule it. Your design would be
+		 * faster if you do batch processing/transmission */
+
+		ret = xsk_ring_prod__reserve(&xsk->tx, 1, &tx_idx);
+		if (ret != 1) {
+			/* No more transmit slots, drop the packet */
+			return false;
+		}
+
+		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->addr = addr;
+		xsk_ring_prod__tx_desc(&xsk->tx, tx_idx)->len = len;
+		xsk_ring_prod__submit(&xsk->tx, 1);
+		xsk->outstanding_tx++;
+
+		xsk->stats.tx_bytes += len;
+		xsk->stats.tx_packets++;
+		return true;
+	}
+
 	return false;
 }
+
+static void handle_receive_packets(struct xsk_socket_info *xsk)
+{
+	unsigned int rcvd, stock_frames, i;
+	uint32_t idx_rx = 0, idx_fq = 0;
+	int ret;
+
+	rcvd = xsk_ring_cons__peek(&xsk->rx, RX_BATCH_SIZE, &idx_rx);
+	if (!rcvd)
+		return;
+
+	/* Stuff the ring with as much frames as possible */
+	stock_frames = xsk_prod_nb_free(&xsk->umem->fq,
+					xsk_umem_free_frames(xsk));
+
+	if (stock_frames > 0) {
+
+		ret = xsk_ring_prod__reserve(&xsk->umem->fq, stock_frames,
+					     &idx_fq);
+
+		/* This should not happen, but just in case */
+		while (ret != stock_frames)
+			ret = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd,
+						     &idx_fq);
+
+		for (i = 0; i < stock_frames; i++)
+			*xsk_ring_prod__fill_addr(&xsk->umem->fq, idx_fq++) =
+				xsk_alloc_umem_frame(xsk);
+
+		xsk_ring_prod__submit(&xsk->umem->fq, stock_frames);
+	}
+
+	/* Process received packets */
+	for (i = 0; i < rcvd; i++) {
+		uint64_t addr = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx)->addr;
+		uint32_t len = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx++)->len;
+
+		if (!process_packet(xsk, addr, len))
+			xsk_free_umem_frame(xsk, addr);
+
+		xsk->stats.rx_bytes += len;
+	}
+
+	xsk_ring_cons__release(&xsk->rx, rcvd);
+	xsk->stats.rx_packets += rcvd;
+
+	/* Do we need to wake up the kernel for transmission */
+	complete_tx(xsk);
+  }
+
+static void rx_and_process(struct config *cfg,
+			   struct xsk_socket_info *xsk_socket)
+{
+
+	udp++;
+
+	struct pollfd fds[2];
+	int ret, nfds = 1;
+
+	memset(fds, 0, sizeof(fds));
+	fds[0].fd = xsk_socket__fd(xsk_socket->xsk);
+	fds[0].events = POLLIN;
+
+	while(!global_exit) {
+		if (cfg->xsk_poll_mode) {
+			ret = poll(fds, nfds, -1);
+			if (ret <= 0 || ret > 1)
+				continue;
+		}
+		handle_receive_packets(xsk_socket);
+	}
+}
+
+#define NANOSEC_PER_SEC 1000000000 /* 10^9 */
+static uint64_t gettime(void)
+{
+	struct timespec t;
+	int res;
+
+	res = clock_gettime(CLOCK_MONOTONIC, &t);
+	if (res < 0) {
+		fprintf(stderr, "Error with gettimeofday! (%i)\n", res);
+		exit(EXIT_FAIL);
+	}
+	return (uint64_t) t.tv_sec * NANOSEC_PER_SEC + t.tv_nsec;
+}
+
+static double calc_period(struct stats_record *r, struct stats_record *p)
+{
+	double period_ = 0;
+	__u64 period = 0;
+
+	period = r->timestamp - p->timestamp;
+	if (period > 0)
+		period_ = ((double) period / NANOSEC_PER_SEC);
+
+	return period_;
+}
+
+static void stats_print(struct stats_record *stats_rec,
+			struct stats_record *stats_prev)
+{
+	uint64_t packets, bytes;
+	double period;
+	double pps; /* packets per sec */
+	double bps; /* bits per sec */
+
+	char *fmt = "%-12s %'11lld pkts (%'10.0f pps)"
+		" %'11lld Kbytes (%'6.0f Mbits/s)"
+		" period:%f\n";
+
+	period = calc_period(stats_rec, stats_prev);
+	if (period == 0)
+		period = 1;
+
+	packets = stats_rec->rx_packets - stats_prev->rx_packets;
+	pps     = packets / period;
+
+	bytes   = stats_rec->rx_bytes   - stats_prev->rx_bytes;
+	bps     = (bytes * 8) / period / 1000000;
+
+	printf(fmt, "AF_XDP RX:", stats_rec->rx_packets, pps,
+	       stats_rec->rx_bytes / 1000 , bps,
+	       period);
+
+	packets = stats_rec->tx_packets - stats_prev->tx_packets;
+	pps     = packets / period;
+
+	bytes   = stats_rec->tx_bytes   - stats_prev->tx_bytes;
+	bps     = (bytes * 8) / period / 1000000;
+
+	printf(fmt, "       TX:", stats_rec->tx_packets, pps,
+	       stats_rec->tx_bytes / 1000 , bps,
+	       period);
+
+	printf("\n");
+}
+
+static void *stats_poll(void *arg)
+{
+	unsigned int interval = 2;
+	struct xsk_socket_info *xsk = arg;
+	static struct stats_record previous_stats = { 0 };
+
+	previous_stats.timestamp = gettime();
+
+	/* Trick to pretty printf with thousands separators use %' */
+	setlocale(LC_NUMERIC, "en_US");
+
+	while (!global_exit) {
+		sleep(interval);
+		xsk->stats.timestamp = gettime();
+		stats_print(&xsk->stats, &previous_stats);
+		previous_stats = xsk->stats;
+	}
+	return NULL;
+}
+
+static void exit_application(int signal)
+{
+	signal = signal;
+	global_exit = true;
+}
+
+/////////////// PACKET PROCESSING FUNCTIONS ///////////////////
+
+void ProcessPacket(unsigned char* buffer, int size) {
+    //Get the IP Header part of this packet
+    struct iphdr *iph = (struct iphdr*)buffer;
+    ++total;
+    switch (iph->protocol) { //Check the Protocol and do accordingly...
+        case 17: //UDP Protocol
+            ++udp;
+            process_udp_packet(buffer , size);
+            break;
+ 
+        default: //Some Other Protocol like ARP etc.
+            ++others;
+            break;
+    }
+}
+ 
 
 void check_pattern(unsigned char *data, int Size) {
     char *pattern = NULL;
@@ -406,182 +607,8 @@ void badCharHeuristic( char *str, int size, int badchar[NO_OF_CHARS])
         badchar[(int) str[i]] = i;
 }                                    
 
-static void handle_receive_packets(struct xsk_socket_info *xsk)
-{
-	unsigned int rcvd, stock_frames, i;
-	uint32_t idx_rx = 0, idx_fq = 0;
-	int ret;
 
-	rcvd = xsk_ring_cons__peek(&xsk->rx, RX_BATCH_SIZE, &idx_rx);
-	if (!rcvd)
-		return;
-
-	pkt_count = rcvd;
-	pkt_dum = 0;
-	
-	/* Stuff the ring with as much frames as possible */
-	stock_frames = xsk_prod_nb_free(&xsk->umem->fq,
-					xsk_umem_free_frames(xsk));
-
-	if (stock_frames > 0) {
-
-		ret = xsk_ring_prod__reserve(&xsk->umem->fq, stock_frames,
-					     &idx_fq);
-
-		/* This should not happen, but just in case */
-		while (ret != stock_frames)
-			ret = xsk_ring_prod__reserve(&xsk->umem->fq, rcvd,
-						     &idx_fq);
-
-		for (i = 0; i < stock_frames; i++)
-			*xsk_ring_prod__fill_addr(&xsk->umem->fq, idx_fq++) =
-				xsk_alloc_umem_frame(xsk);
-
-		xsk_ring_prod__submit(&xsk->umem->fq, stock_frames);
-	}
-
-	int dummy;
-	/* Process received packets */
-	for (i = 0; i < rcvd; i++) {
-		printf("This is packet number %d\n", dummy);
-		uint64_t addr = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx)->addr;
-		uint32_t len = xsk_ring_cons__rx_desc(&xsk->rx, idx_rx++)->len;
-
-		dummy++;
-		if (!process_packet(xsk, addr, len))
-			xsk_free_umem_frame(xsk, addr);
-
-		xsk->stats.rx_bytes += len;
-	}
-
-	xsk_ring_cons__release(&xsk->rx, rcvd);
-	xsk->stats.rx_packets += rcvd;
-
-	/* Do we need to wake up the kernel for transmission */
-	complete_tx(xsk);
-  }
-
-static void rx_and_process(struct config *cfg,
-			   struct xsk_socket_info *xsk_socket)
-{
-	struct pollfd fds[2];
-	int ret, nfds = 1;
-
-	memset(fds, 0, sizeof(fds));
-	fds[0].fd = xsk_socket__fd(xsk_socket->xsk);
-	fds[0].events = POLLIN;
-
-	while(!global_exit) {
-		if (cfg->xsk_poll_mode) {
-			ret = poll(fds, nfds, -1);
-			if (ret <= 0 || ret > 1)
-				continue;
-		}
-		handle_receive_packets(xsk_socket);
-	}
-}
-
-#define NANOSEC_PER_SEC 1000000000 /* 10^9 */
-static uint64_t gettime(void)
-{
-	struct timespec t;
-	int res;
-
-	res = clock_gettime(CLOCK_MONOTONIC, &t);
-	if (res < 0) {
-		fprintf(stderr, "Error with gettimeofday! (%i)\n", res);
-		exit(EXIT_FAIL);
-	}
-	return (uint64_t) t.tv_sec * NANOSEC_PER_SEC + t.tv_nsec;
-}
-
-static double calc_period(struct stats_record *r, struct stats_record *p)
-{
-	double period_ = 0;
-	__u64 period = 0;
-
-	period = r->timestamp - p->timestamp;
-	if (period > 0)
-		period_ = ((double) period / NANOSEC_PER_SEC);
-
-	return period_;
-}
-
-static void stats_print(struct stats_record *stats_rec,
-			struct stats_record *stats_prev)
-{
-	uint64_t packets, bytes;
-	double period;
-	double pps; /* packets per sec */
-	double bps; /* bits per sec */
-
-	char *fmt = "%-12s %'11lld pkts (%'10.0f pps)"
-		" %'11lld Kbytes (%'6.0f Mbits/s)"
-		" period:%f\n";
-
-	period = calc_period(stats_rec, stats_prev);
-	if (period == 0)
-		period = 1;
-
-	packets = stats_rec->rx_packets - stats_prev->rx_packets;
-	pps     = packets / period;
-
-	bytes   = stats_rec->rx_bytes   - stats_prev->rx_bytes;
-	bps     = (bytes * 8) / period / 1000000;
-
-	printf(fmt, "AF_XDP RX:", stats_rec->rx_packets, pps,
-	       stats_rec->rx_bytes / 1000 , bps,
-	       period);
-
-	packets = stats_rec->tx_packets - stats_prev->tx_packets;
-	pps     = packets / period;
-
-	bytes   = stats_rec->tx_bytes   - stats_prev->tx_bytes;
-	bps     = (bytes * 8) / period / 1000000;
-
-	printf(fmt, "       TX:", stats_rec->tx_packets, pps,
-	       stats_rec->tx_bytes / 1000 , bps,
-	       period);
-		   
-	int i;
-	for(i=0; i < pkt_count; i++){
-    	if(stats_rec->match[i] == 0)
-        	printf("No pattern found.");
-    	else
-        	printf("Pattern found.");
-	}
-
-	printf("\n");
-
-
-}
-
-static void *stats_poll(void *arg)
-{
-	unsigned int interval = 2;
-	struct xsk_socket_info *xsk = arg;
-	static struct stats_record previous_stats = { 0 };
-
-	previous_stats.timestamp = gettime();
-
-	/* Trick to pretty printf with thousands separators use %' */
-	setlocale(LC_NUMERIC, "en_US");
-
-	while (!global_exit) {
-		sleep(interval);
-		xsk->stats.timestamp = gettime();
-		stats_print(&xsk->stats, &previous_stats);
-		previous_stats = xsk->stats;
-	}
-	return NULL;
-}
-
-static void exit_application(int signal)
-{
-	signal = signal;
-	global_exit = true;
-}
-
+////////////// END OF PACKET PROCESSING FUNCTIONS ///////////////////
 int main(int argc, char **argv)
 {
 	int ret;
@@ -674,7 +701,6 @@ int main(int argc, char **argv)
 	}
 
 	/* Start thread to do statistics display */
-    // can be created to a thread that prints out if pattern is matched
     // not needed 
 	if (verbose) {
 		ret = pthread_create(&stats_poll_thread, NULL, stats_poll,
@@ -688,7 +714,7 @@ int main(int argc, char **argv)
 
 	/* Receive and count packets than drop them */
     // where to put udp processing
-	rx_and_process(xsk_socket);
+	rx_and_process(&cfg, xsk_socket);
 
 	/* Cleanup */
 	xsk_socket__delete(xsk_socket->xsk);
--- udp_filter.c
+++ udp_filter.c
@@ -1,132 +1,46 @@
-#include <arpa/inet.h>
 #include <linux/bpf.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
-#include <linux/udp.h>
-#include <stdio.h>
-#include <stdlib.h> 
-#include "libbpf/src/bpf_helpers.h"
-# define NO_OF_CHARS 256
+#include <bpf/bpf_helpers.h>
 
-FILE *logfile;
-int udp=0;
-int flag = 0;
-
-void check_pattern(unsigned char *data, int Size);
-int max (int a, int b);
-void badCharHeuristic( char *str, int size, int badchar[NO_OF_CHARS]);
+// map for the af_xdp socket
+struct bpf_map_def SEC("maps") xsks_map = {
+	.type = BPF_MAP_TYPE_XSKMAP,
+	.key_size = sizeof(int),
+	.value_size = sizeof(int),
+	.max_entries = 64,  /* Assume netdev has no more than 64 queues */
+};
 
 SEC("xdp_sock")
-int xdp_func(struct xdp_md *ctx)
+int xdp_sock_prog(struct xdp_md *ctx)
 {
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
-    //char match_pattern[] = "test";
-    unsigned int payload_size, i;
     struct ethhdr *eth = data;
-    unsigned char *payload;
-    struct udphdr *udp;
-    struct iphdr *ip;
+
+    int index = ctx->rx_queue_index;
 
     if (( void *) eth + sizeof (* eth) <= data_end ){
         struct iphdr *ip = data + sizeof (* eth);
         if (( void *) ip + sizeof (* ip) <= data_end ){
-            if (ip -> protocol == IPPROTO_UDP ){
-                udp++;
-                udp = (void *)ip + sizeof(*ip);
+            if (ip -> protocol == IPPROTO_UDP ){ // if protocol is UDP, parse payload
+                if (bpf_map_lookup_elem(&xsks_map, &index)){
+                    return bpf_redirect_map(&xsks_map, index, 0);
+                }
+                /*udp++;
+                struct udphdr *udp = (void *)ip + sizeof(*ip);
                 if ((void *)udp + sizeof(*udp) <= data_end){
                     payload_size = ntohs(udp->len) - sizeof(*udp);
                     payload = (unsigned char *)udp + sizeof(*udp); // start of payload
                     if ((void *)payload + payload_size <= data_end){
                         check_pattern(payload, payload_size);
-                        if (flag == 0){ printf("No pattern found\n"); }
-                        else{ printf("Pattern found\n"); }
+                        if (flag != 0){ rec->matched_packets++; }
                         return XDP_PASS;
+                    
                     }
-                }         
+                } */        
             }
         }
     }   
     return XDP_PASS;
 }
 
-
-void check_pattern(unsigned char *data, int Size) {
-    char *pattern = NULL;
-    FILE *fp;
-    fp = fopen("pats.txt", "r");
-    flag = 0;
-    size_t len = 0;
-    ssize_t read;
-
-    while ((read = getline(&pattern, &len, fp)) != -1) {
-    int m = strlen(pattern);
-    int n = Size;
-
-    int badchar[NO_OF_CHARS];
-
-    /* Fill the bad character array by calling
-        the preprocessing function badCharHeuristic()        
-        for given pattern */
-    badCharHeuristic(pattern, m, badchar);
-
-    int s = 0;  // s is shift of the pattern with
-                    
-    // respect to text
-    while(s <= (n - m))
-    {
-        int j = m-1;
-
-        /* Keep reducing index j of pattern while
-        characters of pattern and text are
-        matching at this shift s */
-        while(j >= 0 && pattern[j] == data[s+j])
-            j--;
-
-        /* If the pattern is present at current
-        shift, then index j will become -1 after
-        the above loop */
-        if (j < 0)
-        {
-            flag = 1;
-            /* Shift the pattern so that the next
-            character in text aligns with the last
-            occurrence of it in pattern.
-            The condition s+m < n is necessary for
-            the case when pattern occurs at the end
-            of text */
-            s += (s+m < n)? m-badchar[data[s+m]] : 1;
-
-        }                             
-        else
-            /* Shift the pattern so that the bad character
-            in text aligns with the last occurrence of
-            it in pattern. The max function is used to
-            make sure that we get a positive shift.
-            We may get a negative shift if the last
-            occurrence  of bad character in pattern
-            is on the right side of the current
-            character. */
-            s += max(1, j - badchar[data[s+j]]);
-    }
-}
-}
-
-// A utility function to get maximum of two integers
-int max (int a, int b) { return (a > b)? a: b; }
-
-// The preprocessing function for Boyer Moore's
-// bad character heuristic
-void badCharHeuristic( char *str, int size, int badchar[NO_OF_CHARS])
-{
-    int i;
-
-    // Initialize all occurrences as -1
-    for (i = 0; i < NO_OF_CHARS; i++)
-        badchar[i] = -1;
-
-    // Fill the actual value of last occurrence
-    // of a character
-    for (i = 0; i < size; i++)
-        badchar[(int) str[i]] = i;
-}                                    
+char _license[] SEC("license") = "GPL";
