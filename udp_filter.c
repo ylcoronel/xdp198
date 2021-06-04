323a582c2207989ec60bd34178253977a6b6aa40
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-06-04 11:51:23 +0800

    basic udp filter with print

--- udp_filter.c
+++ udp_filter.c
@@ -1,102 +1,29 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
-#include <linux/bpf.h>
-
-#include <bpf/bpf_helpers.h>
-
-struct bpf_map_def SEC("maps") xsks_map = {
-	.type = BPF_MAP_TYPE_XSKMAP,
-	.key_size = sizeof(int),
-	.value_size = sizeof(int),
-	.max_entries = 64,  /* Assume netdev has no more than 64 queues */
-};
-
-struct bpf_map_def SEC("maps") xdp_stats_map = {
-	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
-	.key_size    = sizeof(int),
-	.value_size  = sizeof(__u32),
-	.max_entries = 64,
-};
-
-SEC("xdp_sock")
-int xdp_sock_prog(struct xdp_md *ctx)
-{
-    int index = ctx->rx_queue_index;
-    __u32 *pkt_count;
-
-    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
-    if (pkt_count) {
-
-        /* We pass every other packet */
-        if ((*pkt_count)++ & 1)
-            return XDP_PASS;
-    }
-
-    /* A set entry here means that the correspnding queue_id
-     * has an active AF_XDP socket bound to it. */
-    if (bpf_map_lookup_elem(&xsks_map, &index))
-        return bpf_redirect_map(&xsks_map, index, 0);
-
-    return XDP_PASS;
-}
-
 char _license[] SEC("license") = "GPL";
 
-/*#include <linux/bpf.h>
+#include <linux/bpf.h>
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/in.h>
 #include <linux/udp.h>
-#include <bpf/bpf_helpers.h>
-
-// map for the af_xdp socket
-struct bpf_map_def SEC("maps") xsks_map = {
-	.type = BPF_MAP_TYPE_XSKMAP,
-	.key_size = sizeof(int),
-	.value_size = sizeof(int),
-	.max_entries = 64,  /* Assume netdev has no more than 64 queues 
-};
-
-struct bpf_map_def SEC("maps") xdp_stats_map = {
-	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
-	.key_size    = sizeof(int),
-	.value_size  = sizeof(__u32),
-	.max_entries = 64,
-};
 
 SEC("xdp_sock")
-int xdp_sock_prog(struct xdp_md *ctx)
+int udp_filter(struct xdp_md *ctx)
 {
-    void *data_efnd = (void *)(long)ctx->data_end;
+    bpf_trace_printk("got a packet\n");      
     void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
     struct ethhdr *eth = data;
-
-    int index = ctx->rx_queue_index;
-
-    if (( void *) eth + sizeof (* eth) <= data_end ){
-        struct iphdr *ip = data + sizeof (* eth);
-        if (( void *) ip + sizeof (* ip) <= data_end ){
-            if (ip -> protocol == IPPROTO_UDP ){ // if protocol is UDP, parse payload
-                if (bpf_map_lookup_elem(&xsks_map, &index)){
-                    return bpf_redirect_map(&xsks_map, index, 0);
-                }
-                /*udp++;
-                struct udphdr *udp = (void *)ip + sizeof(*ip);
-                if ((void *)udp + sizeof(*udp) <= data_end){
-                    payload_size = ntohs(udp->len) - sizeof(*udp);
-                    payload = (unsigned char *)udp + sizeof(*udp); // start of payload
-                    if ((void *)payload + payload_size <= data_end){
-                        check_pattern(payload, payload_size);
-                        if (flag != 0){ rec->matched_packets++; }
-                        return XDP_PASS;
-                    
-                    }
-                } 
+    if ((void*)eth + sizeof(*eth) <= data_end) {
+        struct iphdr *ip = data + sizeof(*eth);
+        if ((void*)ip + sizeof(*ip) <= data_end) {
+            if (ip->protocol == IPPROTO_UDP) {
+                bpf_trace_printk("udp packet\n");
+                return XDP_PASS;
             }
         }
-    }   
+    }
     return XDP_PASS;
 }
 
-char _license[] SEC("license") = "GPL";*/
+char _license[] SEC("license") = "GPL";
