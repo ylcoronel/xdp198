387e7d8e2c389f9ef51b448ef1e3444fb953501b
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-06-14 23:44:56 +0800

    new code

--- xdp_packetmatch_kern.c
+++ xdp_packetmatch_kern.c
@@ -1,21 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
 #include <linux/bpf.h>
+
 #include <bpf/bpf_helpers.h>
-#include <arpa/inet.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
-#include <linux/udp.h>
 
-#include "common_kern_user.h" /* defines: struct datarec; */
-
-// map for packet count
-struct bpf_map_def SEC("maps") xdp_stats_map = {
-	.type        = BPF_MAP_TYPE_ARRAY,
-	.key_size    = sizeof(__u32),
-	.value_size  = sizeof(struct datarec),
-	.max_entries = XDP_ACTION_MAX,
-};
-
-// map for the af_xdp socket
 struct bpf_map_def SEC("maps") xsks_map = {
 	.type = BPF_MAP_TYPE_XSKMAP,
 	.key_size = sizeof(int),
@@ -23,38 +11,33 @@ struct bpf_map_def SEC("maps") xsks_map = {
 	.max_entries = 64,  /* Assume netdev has no more than 64 queues */
 };
 
-
-#ifndef lock_xadd
-#define lock_xadd(ptr, val)	((void) __sync_fetch_and_add(ptr, val))
-#endif
+struct bpf_map_def SEC("maps") xdp_stats_map = {
+	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
+	.key_size    = sizeof(int),
+	.value_size  = sizeof(__u32),
+	.max_entries = 64,
+};
 
 SEC("xdp_sock")
 int xdp_sock_prog(struct xdp_md *ctx)
 {
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data     = (void *)(long)ctx->data;
-	struct datarec *rec;
-	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
     int index = ctx->rx_queue_index;
+    __u32 *pkt_count;
 
-	/* Lookup in kernel BPF-side return pointer to actual data record */
-	rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
-	if (!rec)
-		return XDP_ABORTED;
+    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
+    if (pkt_count) {
 
-    struct ethhdr *eth = data;
-    if ((void*)eth + sizeof(*eth) <= data_end) {
-        struct iphdr *ip = data + sizeof(*eth);
-        if ((void*)ip + sizeof(*ip) <= data_end) {
-            if (ip->protocol == IPPROTO_UDP) {
-                lock_xadd(&rec->rx_packets, 1);
-                if (bpf_map_lookup_elem(&xsks_map, &index))
-                    return bpf_redirect_map(&xsks_map, index, 0);
-            }
-        }
+        /* We pass every other packet */
+        if ((*pkt_count)++ & 1)
+            return XDP_PASS;
     }
 
-	return XDP_PASS;
+    /* A set entry here means that the correspnding queue_id
+     * has an active AF_XDP socket bound to it. */
+    if (bpf_map_lookup_elem(&xsks_map, &index))
+        return bpf_redirect_map(&xsks_map, index, 0);
+
+    return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
