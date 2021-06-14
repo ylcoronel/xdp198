e84ada4df5ebb9a5f8a2a44220ca8ca549c0a6ee
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-06-15 00:44:52 +0800

    new code

--- xdp_packetmatch_kern.c
+++ xdp_packetmatch_kern.c
@@ -1,43 +1,55 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
 #include <linux/bpf.h>
-
 #include <bpf/bpf_helpers.h>
 
-struct bpf_map_def SEC("maps") xsks_map = {
-	.type = BPF_MAP_TYPE_XSKMAP,
-	.key_size = sizeof(int),
-	.value_size = sizeof(int),
-	.max_entries = 64,  /* Assume netdev has no more than 64 queues */
-};
+#include "common_kern_user.h" /* defines: struct datarec; */
 
+/* Lesson#1: See how a map is defined.
+ * - Here an array with XDP_ACTION_MAX (max_)entries are created.
+ * - The idea is to keep stats per (enum) xdp_action
+ */
 struct bpf_map_def SEC("maps") xdp_stats_map = {
-	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
-	.key_size    = sizeof(int),
-	.value_size  = sizeof(__u32),
-	.max_entries = 64,
+	.type        = BPF_MAP_TYPE_ARRAY,
+	.key_size    = sizeof(__u32),
+	.value_size  = sizeof(struct datarec),
+	.max_entries = XDP_ACTION_MAX,
 };
 
-SEC("xdp_sock")
-int xdp_sock_prog(struct xdp_md *ctx)
+/* LLVM maps __sync_fetch_and_add() as a built-in function to the BPF atomic add
+ * instruction (that is BPF_STX | BPF_XADD | BPF_W for word sizes)
+ */
+#ifndef lock_xadd
+#define lock_xadd(ptr, val)	((void) __sync_fetch_and_add(ptr, val))
+#endif
+
+SEC("xdp_stats1")
+int  xdp_stats1_func(struct xdp_md *ctx)
 {
-    int index = ctx->rx_queue_index;
-    __u32 *pkt_count;
+	// void *data_end = (void *)(long)ctx->data_end;
+	// void *data     = (void *)(long)ctx->data;
+	struct datarec *rec;
+	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
 
-    pkt_count = bpf_map_lookup_elem(&xdp_stats_map, &index);
-    if (pkt_count) {
+	/* Lookup in kernel BPF-side return pointer to actual data record */
+	rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
+	/* BPF kernel-side verifier will reject program if the NULL pointer
+	 * check isn't performed here. Even-though this is a static array where
+	 * we know key lookup XDP_PASS always will succeed.
+	 */
+	if (!rec)
+		return XDP_ABORTED;
 
-        /* We pass every other packet */
-        if ((*pkt_count)++ & 1)
-            return XDP_PASS;
-    }
+	/* Multiple CPUs can access data record. Thus, the accounting needs to
+	 * use an atomic operation.
+	 */
+	lock_xadd(&rec->rx_packets, 1);
+        /* Assignment#1: Add byte counters
+         * - Hint look at struct xdp_md *ctx (copied below)
+         *
+         * Assignment#3: Avoid the atomic operation
+         * - Hint there is a map type named BPF_MAP_TYPE_PERCPU_ARRAY
+         */
 
-    /* A set entry here means that the correspnding queue_id
-     * has an active AF_XDP socket bound to it. */
-    if (bpf_map_lookup_elem(&xsks_map, &index))
-        return bpf_redirect_map(&xsks_map, index, 0);
-
-    return XDP_PASS;
+	return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
