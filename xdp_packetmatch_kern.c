378c592c624c237a3cce485d6455ddec70fc961a
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-06-20 13:52:29 +0800

    new code

--- xdp_packetmatch_kern.c
+++ xdp_packetmatch_kern.c
@@ -1,6 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
 
 #include "common_kern_user.h" /* defines: struct datarec; */
 
@@ -25,8 +29,14 @@ struct bpf_map_def SEC("maps") xdp_stats_map = {
 SEC("xdp_stats1")
 int  xdp_stats1_func(struct xdp_md *ctx)
 {
-	// void *data_end = (void *)(long)ctx->data_end;
-	// void *data     = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
+	unsigned int payload_size;
+	struct ethhdr *eth = data;
+    unsigned char *payload;
+    struct udphdr *udp;
+    struct iphdr *ip;
+
 	struct datarec *rec;
 	
 	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
@@ -48,42 +58,15 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	int k = *rcvdpackets;
 	if(k > 200)
 		lock_xadd(&rec->match, 1);
-        /* Assignment#1: Add byte counters
-         * - Hint look at struct xdp_md *ctx (copied below)
-         *
-         * Assignment#3: Avoid the atomic operation
-         * - Hint there is a map type named BPF_MAP_TYPE_PERCPU_ARRAY
-         */
+    
+	ip = data + sizeof(*eth);
+	udp = (void *)ip + sizeof(*ip);
+	payload_size = ntohs(udp->len) - sizeof(*udp);
+
+	lock_xadd(&rec->payload_size, payload_size);
 
 	return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
 
-/* Copied from: $KERNEL/include/uapi/linux/bpf.h
- *
- * User return codes for XDP prog type.
- * A valid XDP program must return one of these defined values. All other
- * return codes are reserved for future use. Unknown return codes will
- * result in packet drops and a warning via bpf_warn_invalid_xdp_action().
- *
-enum xdp_action {
-	XDP_ABORTED = 0,
-	XDP_DROP,
-	XDP_PASS,
-	XDP_TX,
-	XDP_REDIRECT,
-};
- * user accessible metadata for XDP packet hook
- * new fields must be added to the end of this structure
- *
-struct xdp_md {
-	// (Note: type __u32 is NOT the real-type)
-	__u32 data;
-	__u32 data_end;
-	__u32 data_meta;
-	// Below access go through struct xdp_rxq_info
-	__u32 ingress_ifindex; // rxq->dev->ifindex
-	__u32 rx_queue_index;  // rxq->queue_index
-};
-*/
--- xdp_packetmatch_user.c
+++ xdp_packetmatch_user.c
@@ -123,7 +123,7 @@ static void stats_print(struct stats_record *stats_rec,
 	{
 		char *fmt = "%-12s %'11lld pkts (%'10.0f pps)"
 			//" %'11lld Kbytes (%'6.0f Mbits/s)"
-			" period:%f\n match? %ld\n";
+			" period:%f\n match? %ld\n payload size = %d\n";
 		const char *action = action2str(XDP_PASS);
 		rec  = &stats_rec->stats[0];
 		prev = &stats_prev->stats[0];
@@ -135,7 +135,7 @@ static void stats_print(struct stats_record *stats_rec,
 		packets = rec->total.rx_packets - prev->total.rx_packets;
 		pps     = packets / period;
 
-		printf(fmt, action, rec->total.rx_packets, pps, period, rec->total.match);
+		printf(fmt, action, rec->total.rx_packets, pps, period, rec->total.match, rec->total.payload_size);
 	}
 }
 
@@ -181,6 +181,7 @@ static bool map_collect(int fd, __u32 map_type, __u32 key, struct record *rec)
 	/* Assignment#1: Add byte counters */
 	rec->total.rx_packets = value.rx_packets;
 	rec->total.match = value.match;
+	rec->total.payload_size = value.payload_size;
 	return true;
 }
 
