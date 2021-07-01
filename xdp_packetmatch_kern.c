a35107cd2c5a16eb81be3466e942d11098c6b979
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-07-02 02:45:57 +0800

    new code

--- xdp_packetmatch_kern.c
+++ xdp_packetmatch_kern.c
@@ -81,7 +81,6 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	int j = 0, ctr = 0;
 	
     for (i = 0; i < payload_size; i++){
-        lock_xadd(&rec->rx_packets, 1);
         if (payload[i] == match_pattern[j]){
 			j++;
 		}else if(payload[i] != match_pattern[j]){
@@ -93,6 +92,10 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 			return XDP_PASS;
 		}
 	}
+
+    if(i > 0){
+        lock_xadd(&rec->rx_packets, 1);
+    }
 
 	if(ctr > 0){
 		lock_xadd(&rec->match, 1);
