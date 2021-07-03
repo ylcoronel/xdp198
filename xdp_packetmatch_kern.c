3e898926e7c3bb5baca77f869ddd9ebc8c80ffd3
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-07-03 16:34:34 +0800

    new code

--- xdp_packetmatch_kern.c
+++ xdp_packetmatch_kern.c
@@ -81,7 +81,7 @@ int  xdp_stats1_func(struct xdp_md *ctx)
 	int j = 0, ctr = 0;
 	
 	#pragma clang loop unroll_count(1)
-    for (i = 0; i < 512; i++){
+    for (i = 0; i < 5; i++){
         if (payload[i] == match_pattern[j]){
 			j++;
 		}else if(payload[i] != match_pattern[j]){
