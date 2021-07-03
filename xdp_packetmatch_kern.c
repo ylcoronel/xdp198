abb22083012db52e81ed83b437081becc32a702c
Ysabela <ysabela.louise.coronel@eee.upd.edu.ph>
2021-07-04 00:43:22 +0800

    new code

--- xdp_packetmatch_kern.c
+++ xdp_packetmatch_kern.c
@@ -8,24 +8,19 @@
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/udp.h>
-#include <linux/module.h>  
-#include <linux/kernel.h>
 
 #include "common_kern_user.h" /* defines: struct datarec; */
 
+/* Lesson#1: See how a map is defined.
+ * - Here an array with XDP_ACTION_MAX (max_)entries are created.
+ * - The idea is to keep stats per (enum) xdp_action
+ */
 struct bpf_map_def SEC("maps") xdp_stats_map = {
 	.type        = BPF_MAP_TYPE_ARRAY,
 	.key_size    = sizeof(__u32),
 	.value_size  = sizeof(struct datarec),
 	.max_entries = XDP_ACTION_MAX,
 };
-
-#define printk(fmt, ...)					            \
-({								                        \
-	       char ____fmt[] = fmt;				        \
-	       bpf_trace_printk(____fmt, sizeof(____fmt),	\
-				##__VA_ARGS__);			                \
-})
 
 /* LLVM maps __sync_fetch_and_add() as a built-in function to the BPF atomic add
  * instruction (that is BPF_STX | BPF_XADD | BPF_W for word sizes)
@@ -35,13 +30,12 @@ struct bpf_map_def SEC("maps") xdp_stats_map = {
 #endif
 
 SEC("xdp_stats1")
-int xdp_stats1_func(struct xdp_md *ctx)
+int  xdp_stats1_func(struct xdp_md *ctx)
 {
-    printk("Hello\n");  
 
 	struct datarec *rec;
 	
-	__u32 key = XDP_PASS; 
+	__u32 key = XDP_PASS; /* XDP_PASS = 2 */
 
 	/* Lookup in kernel BPF-side return pointer to actual data record */
 	rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
@@ -51,14 +45,12 @@ int xdp_stats1_func(struct xdp_md *ctx)
 
 	void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
-    char match_pattern[] = "test"; 
-
-    unsigned int payload_size;
+    char match_pattern[] = "FJDMFOEOLTUUWU";
+    unsigned int payload_size, i;
     struct ethhdr *eth = data;
     unsigned char *payload;
     struct udphdr *udp;
     struct iphdr *ip;
-    int i, match;
 
     if ((void *)eth + sizeof(*eth) > data_end)
         return XDP_PASS;
@@ -86,17 +78,33 @@ int xdp_stats1_func(struct xdp_md *ctx)
         return XDP_PASS;
 	}
 
-    rec->rx_packets++;
-    
-	for (i = 0; i < 512; i++){
-        if (payload[i] != match_pattern[i]){
-            match = 0;
-        }else{
-            match = 1;
-        }
-    }
-      
-    return XDP_PASS;
+	int j = 0, ctr = 0;
+	
+	#pragma clang loop unroll_count(1)
+    for (i = 0; i < 5; i++){
+        if (payload[i] == match_pattern[j]){
+			j++;
+		}else if(payload[i] != match_pattern[j]){
+			j = 0;
+		}
+
+		if(j == sizeof(match_pattern)-1){
+			ctr = ctr + 1;
+			return XDP_PASS;
+		}
+	}
+
+	if(i>0){
+		lock_xadd(&rec->rx_packets, 1);
+	}
+
+
+	if(ctr > 0){
+		lock_xadd(&rec->match, 1);
+	}
+
+
+	return XDP_PASS;
 }
 
 char _license[] SEC("license") = "GPL";
